92bc04e0da7cb41f81bf0c920b869369a78d0e63
Greg Kezos <greg.kezos@gmail.com>
2016-11-19 11:09:02 -0700

    working page

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require ('mongoose');
-var Profile = require('../models/movie');
+var Profile = require('../models/profile');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  Profile.find( function(err, movies) {
-    res.send('profiles', {movies: movies});
+  Profile.find( function(err, profiles) {
+    res.send('profiles', {profiles: profiles});
   });
 });
 
@@ -15,21 +15,21 @@ router.post('/', function (req, res, next) {
     title: req.body.title,
     description: req.body.description,
     updatedAt: Date.now()
-  }).save( function(err, movie) {
-    res.redirect('/movies');
+  }).save( function(err, profile) {
+    res.redirect('/profiles');
   });
 });
 
 router.post('/:id', function (req, res) {
   Profile.finById(req.params.id, function(err, profile) {
     profile.remove( function() {
-      res.redirect('/movies');
+      res.redirect('/profiles');
     });
   });
 });
 
 router.post('/edit/:id', function (req, res) {
-  Profile.findById ( req.params.id, function(err, movie) {
+  Profile.findById ( req.params.id, function(err, profile) {
     profile.title = req.body.title;
     profile.description = req.body.description;
     profile.updatedAt = Date.now();
