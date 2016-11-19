81c7027f95973a8d73b0a3a16e78ca55a8bd6fb7
Greg Kezos <greg.kezos@gmail.com>
2016-11-19 10:25:01 -0700

    Server side barebones

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require ('mongoose');
+var Profile = require('../models/movie');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Profile.find( function(err, movies) {
+    res.send('profiles', {movies: movies});
+  });
 });
+
+router.post('/', function (req, res, next) {
+  new Profile({
+    title: req.body.title,
+    description: req.body.description,
+    updatedAt: Date.now()
+  }).save( function(err, movie) {
+    res.redirect('/movies');
+  });
+});
+
+router.post('/:id', function (req, res) {
+  Profile.finById(req.params.id, function(err, profile) {
+    profile.remove( function() {
+      res.redirect('/movies');
+    });
+  });
+});
+
+router.post('/edit/:id', function (req, res) {
+  Profile.findById ( req.params.id, function(err, movie) {
+    profile.title = req.body.title;
+    profile.description = req.body.description;
+    profile.updatedAt = Date.now();
+    profile.save( function() {
+      res.redirect('/profiles');
+    });
+  });
+});
+
 
 module.exports = router;
