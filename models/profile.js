57c446431df440dd841717e71bd489c8e33d0e60
Jeff Verhaaren <jeffsverhaaren@gmail.com>
2016-11-19 13:08:45 -0700

    working index and profiles page

--- models/profile.js
+++ models/profile.js
@@ -1,18 +1,16 @@
-/*var mongoose = require('mongoose');
-var Schema = mongoose.Schema;*/
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/band-together');
-/*var Profile = require('./models/profile');
+var Schema = mongoose.Schema
 
 var Profile = new Schema({
   name: String,
   age: Number,
   area: String,
-  instruments: String
-  experience: Number
+  instruments: String,
+  experience: Number,
   style: String,
   needs: String,
   updatedAt: Date
 });
 
-module.exports = mongoose.model('Profile', Profile);*/
+module.exports = mongoose.model('Profile', Profile);
